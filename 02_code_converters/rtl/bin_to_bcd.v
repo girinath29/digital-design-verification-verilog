144bf412961f3d346d270dee1e59aca46f5b5586
girinath29 <girinathgoruva29@gmail.com>
2026-02-10 10:59:48 +0530

    Update bin_to_bcd.v

--- 02_code_converters/rtl/bin_to_bcd.v
+++ 02_code_converters/rtl/bin_to_bcd.v
@@ -1,19 +1,27 @@
-module binary2bcd(input [7:0]Bin, output reg [11:0] Bout);
-  reg [19:0] shift_reg;
-  integer i;
-  
-  always @(*)
-    begin
-      shift_reg = 0;
-      shift_reg = Bin;
-      
-      for(i=0; i<8;i++)
-        begin
-          if(shift_reg[11:8] >=5) shift_reg[11:8] = shift_reg[11:8] +3;
-          if(shift_reg[15:12] >=5) shift_reg[15:12] = shift_reg[15:12] +3;
-          if(shift_reg[19:16] >=5) shift_reg[11:8] = shift_reg[19:16]+3;
-          shift_reg = shift_reg <<1;
-                       end
-                       Bout = shift_reg [19:8];
-                       end
-                       endmodule
+module bin_to_bcd_param #(
+    parameter integer BIN_W  = 16,   // Binary width (e.g., 16 bits)
+    parameter integer DIGITS = 5     // Number of BCD digits (e.g., 5 digits)
+)(
+    input  wire [BIN_W-1:0]    bin,
+    output reg  [DIGITS*4-1:0] bcd
+);
+    integer i, d;
+    reg [BIN_W + DIGITS*4 - 1 : 0] shift_reg;
+
+    always @(*) begin
+        shift_reg = { (BIN_W + DIGITS*4){1'b0} };
+        shift_reg[BIN_W-1:0] = bin;
+
+        // Double-Dabble (Shift-Add-3)
+        for (i = 0; i < BIN_W; i = i + 1) begin
+            for (d = 0; d < DIGITS; d = d + 1) begin
+                if (shift_reg[BIN_W + d*4 +: 4] >= 5)
+                    shift_reg[BIN_W + d*4 +: 4] =
+                        shift_reg[BIN_W + d*4 +: 4] + 4'd3;
+            end
+            shift_reg = shift_reg << 1;
+        end
+
+        bcd = shift_reg[BIN_W + DIGITS*4 - 1 : BIN_W];
+    end
+endmodule
